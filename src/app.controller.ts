6e5283bd0d6a5d674ff27d6c3554b8434930725d
MrSchwartz01 <xavierferrin01@gmail.com>
2026-01-28 10:42:10 -0500

    actualizacion configuracion controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Header('Access-Control-Allow-Origin', '*')
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('secure')
+  @Header('Access-Control-Allow-Origin', 'frontend-chpc.vercel.app')
+  @Header('Access-Control-Allow-Credentials', 'true')
+  getSecureData(): { data: string } {
+    return { data: 'This is secure' };
   }
 }
